240541fd1fb1e58970565d3d4b22585a6086f256
kolasaniv1996 <92003218+kolasaniv1996@users.noreply.github.com>
2024-07-24 14:58:22 -0400

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Set the environment variables
-export MASTER_ADDR=$MASTER_ADDR
-export MASTER_PORT=$MASTER_PORT
-export WORLD_SIZE=$WORLD_SIZE
+master_addr=$MASTER_ADDR
+master_port=$MASTER_PORT
+job_n=$WORLD_SIZE
+#job_id=$RANK
 # export RANK=$RANK  # Uncomment if you need to use RANK explicitly
 
 # Optional: Print the variables for debugging
@@ -13,4 +13,5 @@ echo "WORLD_SIZE: ${WORLD_SIZE}"
 # echo "RANK: ${RANK}"
 
 # Run the training script using torchrun
-torchrun --nproc_per_node=1 --nnodes=${WORLD_SIZE} --rdzv_endpoint=${MASTER_ADDR}:${MASTER_PORT} --rdzv_backend=c10d camelyon.py --batch_size 8 30 2
+LOGLEVEL="INFO" torchrun --nproc_per_node=1 --nnodes=2:${job_n} --max-restarts=3 --rdzv_endpoint=${MASTER_ADDR}:${MASTER_PORT} --rdzv_backend=c10d camelyon.py --batch_size 8 1000 2
+
